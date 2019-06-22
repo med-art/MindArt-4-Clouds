81203747d98796c3dfa3fde429dd87f8b3e0cec6
Cameron May <camerondmay@gmail.com>
2019-06-22 23:01:26 +1200

    Update p5Sketch.js

--- p5Sketch.js
+++ p5Sketch.js
@@ -3,12 +3,15 @@
   let brush = [];
 
   // bursh mechanics
-  let angle1, segLength,
-    rake3X = 0,
-    rake3Y = 0;
+  let angle1, segLength;
   let scalar = 0;
   let tempMouseX = 0;
   let tempMouseY = 0;
+  let tempX = 100;
+  let tempY = 100;
+  let dx;
+  let dy;
+
 
   // VARIABLES FOR TIME DELAY ON BRUSH
   let milliCounter;
@@ -18,13 +21,26 @@
   let milliComp = 10;
   let scatterAmount = 2;
 
+
   // COLOUR VARAIABLES
   let colHue;
-  const colHueMax = 255;
+  const colHueMin = 255;
+  const colHueMax = 0;
+
   let colSat;
-  const colSatMax = 120;
+  const colSatMin = 50;
+  const colSatMax = 200;
+
   let colBri;
-  const colBriMax = 255;
+  const colBriMin = 190;
+  const colBriMax = 250;
+
+  let colOpacity = 0.4;
+
+  const hueDrift = 3;
+  const satDrift = 3;
+  const rotateDrift = 0.2;
+
 
   // STATE SWITCH
   let bool = 1;
@@ -33,56 +49,46 @@
   //button spacing
   //margin from right
 
-
-
-
   function preload() {
-
-    bg = loadImage('assets/paper.jpg');
-
+    bg = loadImage('assets/paper.jpg'); // background paper
     for (i = 1; i < 21; i++) {
-      brush[i] = loadImage('assets/br-' + i + '.png')
+      brush[i] = loadImage('assets/br-' + i + '.png') // brush loader
     }
   }
 
   function setup() {
     createCanvas(windowWidth, windowHeight);
-    imageMode(CENTER); // centers loaded brushes
     pixelDensity(1); // Ignores retina displays
-    blendMode(BLEND);
 
-    // smooth();
-
-    image(bg, width / 2, height / 2, width, height);
-
-    segLength = width / 15;
-
+    imageMode(CENTER); // centers loaded brushes
+    blendMode(BLEND); // consider overlay and multiply
     colorMode(HSB, 255, 255, 255, 1)
+    colHue = random(colHueMin, colHueMax);
+    colSat = random(colSatMin, colSatMax);
+    colBri = random(colBriMin, colBriMax)
 
-    colHue = random(0, 255);
-    colSat = random(50, 180);
-    colBri = random(0, 255);
+    image(bg, width / 2, height / 2, width, height); // display backgrond
 
-    strokeWeight(5);
-    stroke(255, 0, 255);
+    segLength = width / 15; // length of delay between touch and paint or line // 15 is a good value
+
+    strokeWeight(4); // for line work
+    stroke(255, 0, 255); // for line work
   }
 
   function keyPressed() {
-    bool = !bool;
+    bool = !bool; // temporary invert function, will be overidden by UI
   }
 
-  function draw() {
-    milliCounter = millis();
-  }
+
 
   function mousePressed() {
+
     tempMouseX = ((width / 2) - mouseX); // record position on downpress
     tempMouseY = ((height / 2) - mouseY); // record position on downpress
     brushTemp = int(random(1, 20));
     tint(255, 0.03); // Display at half opacit
 
     if (bool) {
-
       image(bg, width / 2, height / 2, width, height);
     }
   }
@@ -90,36 +96,38 @@
 
   function mouseDragged() {
 
+    milliCounter = millis();
+
     if (bool) {
 
       if (milliCounter > milliTrack + milliComp) {
-        tint((colHue += random(-3, 3)), (colSat += random(-3, 3)), 220, 0.4); // Display at half opacity
+        tint((colHue += random(-hueDrift, hueDrift)), (colSat += random(-satDrift, satDrift)), colBri, colOpacity); // Display at half opacity
 
-        console.log(colHue);
-        dx = mouseX - rake3X;
-        dy = mouseY - rake3Y;
-        angle1 = atan2(dy, dx) + (random(-0.5, 0.5));
-        rake3X = mouseX - (cos(angle1) * (segLength / 2));
-        rake3Y = mouseY - (sin(angle1) * (segLength / 2));
+        if (colSat < 10) {
+          colSat += 30
+        }
+
+        console.log('Hue = '+colHue);
+        console.log('Saturation = '+colSat);
+        console.log('Brightness = '+colBri);
+
+        dx = mouseX - tempX;
+        dy = mouseY - tempY;
+
+        angle1 = atan2(dy, dx) + (random(-rotateDrift, rotateDrift)); // https://p5js.org/reference/#/p5/atan2
+        tempX = mouseX - (cos(angle1) * segLength/2); // https://p5js.org/examples/interaction-follow-1.html
+        tempY = mouseY - (sin(angle1) * segLength/2);
         scalar = random(0, 2 * (width - (abs(mouseX - pmouseX))) / width);
-        console.log(scalar);
 
-        segment(rake3X, rake3Y, angle1, brush[brushTemp], scalar)
+
+        segment(tempX, tempY, angle1, brush[brushTemp], scalar)
 
         milliTrack = milliCounter;
-
-
       }
     } else {
-
-
       line(mouseX, mouseY, pmouseX, pmouseY);
     }
-
-
   }
-
-
 
   function segment(rakeX, rakeY, a, rake, scalar) {
     push();
@@ -134,5 +142,5 @@
 
 
   function windowResized() {
-    setup();
+    setup(); // need to rewrite this to ensure image is saved
   }
