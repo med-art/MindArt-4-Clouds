d0ddeb580a5e49f452b6b4d926b11a97777f41e3
Cameron May <camerondmay@gmail.com>
2019-07-02 15:21:19 +1200

    edit

--- p5Sketch.js
+++ p5Sketch.js
@@ -283,20 +283,4 @@ preventPullToRefresh('#id') // pass #id or html tag into the method
     setup(); // need to rewrite this to ensure image is saved
   }
 
-  function preventPullToRefresh(element) {
-    var prevent = false;
-
-    document.querySelector(element).addEventListener('touchstart', function(e){
-      if (e.touches.length !== 1) { return; }
-
-      var scrollY = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
-      prevent = (scrollY === 0);
-    });
-
-    document.querySelector(element).addEventListener('touchmove', function(e){
-      if (prevent) {
-        prevent = false;
-        e.preventDefault();
-      }
-    });
-  }
+  
--- preventMove.js
+++
@@ -1,3 +0,0 @@
-document.body.addEventListener('touchmove',function(event){
- event.preventDefault();
-});
