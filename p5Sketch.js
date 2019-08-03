6758af30f1224a3c5973aeb487db85baf6ece957
Cameron May <camerondmay@gmail.com>
2019-08-04 08:19:21 +1200

    Major revision to UI

--- p5Sketch.js
+++ p5Sketch.js
@@ -86,11 +86,14 @@
     for (i = 1; i < 21; i++) {
       brush[i] = loadImage('assets/br-' + i + '.png') // brush loader
     }
+
+    audio = loadSound('assets/audio.mp3');
+
   }
 
   function setup() {
-createCanvas(windowWidth, windowHeight);
-dimensionCalc();
+    createCanvas(windowWidth, windowHeight);
+    dimensionCalc();
 
     pixelDensity(1); // Ignores retina displays
 
@@ -101,7 +104,7 @@ dimensionCalc();
     colSat = random(colSatMin, colSatMax);
 
 
-  backdrop();
+    backdrop();
 
     segLength = windowWidth / 40; // length of delay between touch and paint or line // 15 is a good value
 
@@ -111,9 +114,9 @@ dimensionCalc();
     writeTextUI();
   }
 
-  function backdrop(){
-      noTint();
-      image(bg, windowWidth / 2, windowHeight / 2, windowWidth, windowHeight); // display backgrond
+  function backdrop() {
+    noTint();
+    image(bg, windowWidth / 2, windowHeight / 2, windowWidth, windowHeight); // display backgrond
   }
 
   function dimensionCalc() {
@@ -139,8 +142,7 @@ dimensionCalc();
       button1A.style('color', 'grey');
       button1B.style('background-color', colSelect);
       button1B.style('color', 'white');
-    }
-    else {
+    } else {
       button1A.style('background-color', colSelect);
       button1A.style('color', 'white');
       button1B.style('background-color', col);
@@ -157,8 +159,7 @@ dimensionCalc();
       button2A.style('color', 'white');
       button2B.style('background-color', col);
       button2B.style('color', 'grey');
-    }
-    else {
+    } else {
       button2A.style('background-color', col);
       button2A.style('color', 'grey');
       button2B.style('background-color', colSelect);
@@ -167,9 +168,9 @@ dimensionCalc();
     }
   }
 
-  function writeTextUI(){
+  function writeTextUI() {
 
-    textSize(windowWidth/50);
+    textSize(windowWidth / 50);
     fill(0);
     noStroke();
 
@@ -180,49 +181,49 @@ dimensionCalc();
     button2B = createButton('Trace');
     button3 = createButton('New drawing');
 
-    button1A.position(lmax,windowHeight-lmax*5);
-    button1B.position((lmax*15)+lmax,windowHeight-lmax*5); // 16 because 16 characters in 'Sunset Colours'
-    button2A.position(lmax,windowHeight-lmax*8);
-    button2B.position(lmax*12.5+lmax,windowHeight-lmax*8); // 7 because 7 characters in Paint
+    button1A.position(lmax, windowHeight - lmax * 5);
+    button1B.position((lmax * 15) + lmax, windowHeight - lmax * 5); // 16 because 16 characters in 'Sunset Colours'
+    button2A.position(lmax, windowHeight - lmax * 10);
+    button2B.position(lmax * 12.5 + lmax, windowHeight - lmax * 10); // 7 because 7 characters in Paint
 
-    button3.position(windowWidth-(10*lmax)-(lmax*3),windowHeight-lmax*4);
+    button3.position(windowWidth - (20 * lmax) - (lmax * 3), windowHeight - lmax * 5);
 
-   col = color(0,0,0,0.2);
-   colSelect = color(0,0,0,0.7);
-colH3 = color(355,87,74);
+    col = color(0, 0, 0, 0.2);
+    colSelect = color(0, 0, 0, 0.7);
+    colH3 = color(355, 87, 74);
 
-   button1A.style('background-color', colSelect)
-   button1A.style('font-size', '1.5vmax');
-   button1A.style('color', 'white');
-   button1A.style('width', '15vmax');
-   button1A.mousePressed(invertColourSet);
+    button1A.style('background-color', colSelect)
+    button1A.style('font-size', '2vmax');
+    button1A.style('color', 'white');
+    button1A.style('width', '15vmax');
+    button1A.mousePressed(invertColourSet);
 
-   button1B.style('background-color', col)
-   button1B.style('font-size', '1.5vmax');
-   button1B.style('color', 'grey');
-   button1B.style('width', '15vmax');
-   button1B.mousePressed(invertColourSet);
+    button1B.style('background-color', col)
+    button1B.style('font-size', '2vmax');
+    button1B.style('color', 'grey');
+    button1B.style('width', '15vmax');
+    button1B.mousePressed(invertColourSet);
 
-   button2A.style('background-color', colSelect)
-   button2A.style('font-size', '2vmax');
-   button2A.style('color', 'white');
-   button2A.mousePressed(invertTracing);
-   button2A.style('border-radius', '0.25vmax')
-   button2A.style('width', '12.5vmax');
+    button2A.style('background-color', colSelect)
+    button2A.style('font-size', '2.5vmax');
+    button2A.style('color', 'white');
+    button2A.mousePressed(invertTracing);
+    button2A.style('border-radius', '0.25vmax')
+    button2A.style('width', '12.5vmax');
 
-   button2B.style('background-color', col)
-   button2B.style('font-size', '2vmax');
-   button2B.style('color', 'grey');
-   button2B.style('border-radius', '0.25vmax')
-   button2B.style('width', '12.5vmax');
-   button2B.mousePressed(invertTracing);
+    button2B.style('background-color', col)
+    button2B.style('font-size', '2.5vmax');
+    button2B.style('color', 'grey');
+    button2B.style('border-radius', '0.25vmax')
+    button2B.style('width', '12.5vmax');
+    button2B.mousePressed(invertTracing);
 
-   button3.style('background-color', colH3);
-   button3.style('font-size', '1.25vmax');
-   button3.style('color', 'white');
-   button3.style('border-radius', '0.25vmax')
-    button3.style('width', '10vmax')
-   button3.mousePressed(reset);
+    button3.style('background-color', colH3);
+    button3.style('font-size', '2.5vmax');
+    button3.style('color', 'white');
+    button3.style('border-radius', '0.25vmax')
+    button3.style('width', '20vmax')
+    button3.mousePressed(reset);
 
 
   }
@@ -232,6 +233,12 @@ colH3 = color(355,87,74);
 
   function mousePressed() {
 
+    if (audio.isPlaying()) {
+      // do nothing
+    } else {
+      audio.play();
+    }
+
     tempwinMouseX = ((windowWidth / 2) - winMouseX); // record position on downpress
     tempwinMouseY = ((windowHeight / 2) - winMouseY); // record position on downpress
     brushTemp = int(random(1, 20));
@@ -239,15 +246,14 @@ colH3 = color(355,87,74);
 
     if (bool) {
       image(bg, windowWidth / 2, windowHeight / 2, windowWidth, windowHeight);
-      let swatchTemp = int(random(0,5));
+      let swatchTemp = int(random(0, 5));
       //console.log(swatchTemp);
 
       if (colourBool) {
         colHue = cloudHSB[swatchTemp][0];
         colSat = cloudHSB[swatchTemp][1];
         colBri = cloudHSB[swatchTemp][2];
-      }
-      else  {
+      } else {
         colHue = sunsetHSB[swatchTemp][0];
         colSat = sunsetHSB[swatchTemp][1];
         colBri = sunsetHSB[swatchTemp][2];
@@ -271,17 +277,17 @@ colH3 = color(355,87,74);
           colSat += 30
         }
 
-        console.log('Hue = '+colHue);
-        console.log('Saturation = '+colSat);
-        console.log('Brightness = '+colBri);
+        console.log('Hue = ' + colHue);
+        console.log('Saturation = ' + colSat);
+        console.log('Brightness = ' + colBri);
 
         dx = winMouseX - tempX;
         dy = winMouseY - tempY;
 
         angle1 = atan2(dy, dx) + (random(-rotateDrift, rotateDrift)); // https://p5js.org/reference/#/p5/atan2
-        tempX = winMouseX - (cos(angle1) * segLength/2); // https://p5js.org/examples/interaction-follow-1.html
-        tempY = winMouseY - (sin(angle1) * segLength/2);
-        scalar = constrain(100*(random(3,abs(winMouseX - pwinMouseX))/windowWidth),0,1.5);
+        tempX = winMouseX - (cos(angle1) * segLength / 2); // https://p5js.org/examples/interaction-follow-1.html
+        tempY = winMouseY - (sin(angle1) * segLength / 2);
+        scalar = constrain(100 * (random(3, abs(winMouseX - pwinMouseX)) / windowWidth), 0, 1.5);
 
 
         segment(tempX, tempY, angle1, brush[brushTemp], scalar)
@@ -289,7 +295,7 @@ colH3 = color(355,87,74);
         milliTrack = milliCounter;
       }
     } else {
-      strokeWeight(constrain(abs((winMouseY+winMouseX)-(pwinMouseX+pwinMouseY)),0.3,2)); // for line work
+      strokeWeight(constrain(abs((winMouseY + winMouseX) - (pwinMouseX + pwinMouseY)), 0.3, 2)); // for line work
       stroke(255, 0, 255); // for line work
       line(winMouseX, winMouseY, pwinMouseX, pwinMouseY);
     }
@@ -298,8 +304,8 @@ colH3 = color(355,87,74);
   function segment(rakeX, rakeY, a, rake, scalar) {
     push();
 
-    translate(rakeX + (randomGaussian(-scatterAmount*(0.1*scalar), scatterAmount*(0.1*scalar))), rakeY + (randomGaussian(-scatterAmount*(0.1*scalar), scatterAmount*(0.1*scalar))));
-//translate(rakeX, rakeY);
+    translate(rakeX + (randomGaussian(-scatterAmount * (0.1 * scalar), scatterAmount * (0.1 * scalar))), rakeY + (randomGaussian(-scatterAmount * (0.1 * scalar), scatterAmount * (0.1 * scalar))));
+    //translate(rakeX, rakeY);
     rotate(a);
     scale(scalar);
 
@@ -310,7 +316,7 @@ colH3 = color(355,87,74);
   function reset() {
     backdrop();
     if (!bool)
-   invertTracing();
+      invertTracing();
   }
 
 
