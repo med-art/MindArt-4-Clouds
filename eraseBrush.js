ee5aeafeb27b6dfa87c277099f3e6b81eb12c722
Cameron May <camerondmay@gmail.com>
2019-08-05 23:47:32 +1200

    Update eraseBrush.js

--- eraseBrush.js
+++ eraseBrush.js
@@ -2,11 +2,13 @@ function eraseDrawing() {
 
   if (bool) {
     paintLayer.loadPixels();
-    for (let y = (winMouseY - 25); y < (winMouseY + 25); y++) {
-      for (let x = (winMouseX - 25); x < (winMouseX + 25); x++) {
+    for (let y = (winMouseY - 50); y < (winMouseY + 50); y+=5) {
+      for (let x = (winMouseX - 50); x < (winMouseX + 50); x+=5) {
+        if (dist(x, y, winMouseX, winMouseY) < 50) {
           let index = (x + y * width) * 4;
           paintLayer.pixels[index + 3] = 0; // alpha channel
         }
+      }
     }
     paintLayer.updatePixels();
   } else {
