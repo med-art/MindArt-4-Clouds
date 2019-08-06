c19a4889618118a8b6f5d4a5eb953911ab8e7ec3
Cameron May <camerondmay@gmail.com>
2019-08-06 21:00:47 +1200

    Update eraseBrush.js

--- eraseBrush.js
+++ eraseBrush.js
@@ -2,8 +2,8 @@ function eraseDrawing() {
 
   if (bool) {
     paintLayer.loadPixels();
-    for (let y = (winMouseY - 50); y < (winMouseY + 50); y+=4) {
-      for (let x = (winMouseX - 50); x < (winMouseX + 50); x+=4) {
+    for (let y = (winMouseY - 50); y < (winMouseY + 50); y++) {
+      for (let x = (winMouseX - 50); x < (winMouseX + 50); x++) {
         if (dist(x, y, winMouseX, winMouseY) < 50) {
           let index = (x + y * width) * 4;
           paintLayer.pixels[index + 3] = 0; // alpha channel
