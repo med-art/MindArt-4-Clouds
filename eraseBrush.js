a0de46b71f538a432666c4386b1fbea96391bc29
Cameron May <camerondmay@gmail.com>
2019-11-19 08:09:23 +1300

    Update eraseBrush.js

--- eraseBrush.js
+++ eraseBrush.js
@@ -11,7 +11,7 @@ function eraseDrawing() {
 
         else {
           traceLayer.blendMode(BLEND);
-          traceLayer.strokeWeight(40);
+          traceLayer.strokeWeight(100);
           traceLayer.stroke(255,0,0,0.4);
           traceLayer.line(mouseX, mouseY, pmouseX, pmouseY);
           traceLayer.blendMode(LIGHTEST);
