29aebd4343a3fcd51e836b27bc254f0b82c0b126
Cameron May <camerondmay@gmail.com>
2019-11-19 07:39:32 +1300

    EraseUpdated

--- eraseBrush.js
+++ eraseBrush.js
@@ -4,7 +4,7 @@ function eraseDrawing() {
   if (eraserVersion) {
 
     paintLayer.noStroke();
-    paintLayer.fill(255,0,255,0.1);
+    paintLayer.fill(255,0,255,0.4);
     paintLayer.circle(mouseX, mouseY, 50, 50);
 
         }
@@ -12,7 +12,7 @@ function eraseDrawing() {
         else {
           traceLayer.blendMode(BLEND);
           traceLayer.noStroke();
-          traceLayer.fill(255,0,0,0.1);
+          traceLayer.fill(255,0,0,0.4);
           traceLayer.circle(mouseX, mouseY, 40, 40);
           traceLayer.blendMode(LIGHTEST);
 
